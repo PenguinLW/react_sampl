5e819c1d68373933a5c5479c6af040f6d1c80f03
Kovalsky95 <diana1997525@gmail.com>
2021-06-23 02:35:35 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 #git config --global user.email "diana1997525@gmail.com"
 #git config --global user.name "Kovalsky95"
 #git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=3600"
+git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
 git pull --all
