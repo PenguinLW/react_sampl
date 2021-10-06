e51bb6150f6761768fe2205b26de82a0d3665430
Kovalsky95 <diana1997525@gmail.com>
2021-10-06 19:40:31 +0800

    PenguinL

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
 #in venv
-. venv/bin/activate
+. venv/bin/activate #. venv/bin/activate &&. nenv/bin/activate
 #from venv python this project
 python -m pip install --upgrade pip
 pip install nodeenv
@@ -7,9 +7,9 @@ nodeenv nenv
 
 . nenv/bin/activate
 
-#cd ..
 npx create-react-app react_sampl
 
 cd react_sampl
 npm start index.js
+#deactivate
 #deactivate_node
