c49bdc57fbe5b7cd09ec71f12206fda7c9d60662
PenguinLW <diana1997525@gmail.com>
2023-04-10 19:20:21 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 #pip install -r req.txt
 pip freeze > req.txt
 chmod u+x *.sh *.py
-#git clone -o react_sampl https://github.com/Kovalsky95/react_sampl.git
+#git clone -o react_sampl https://github.com/PenguinLW/react_sampl.git
 #git config --global user.email "diana1997525@gmail.com"
 #git config --global user.name "Kovalsky95"
 #git config --global credential.helper cache
