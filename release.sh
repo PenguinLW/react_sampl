6d5642ef3beab5165047d02d7b81c343e25c50dd
PenguinLW <diana1997525@gmail.com>
2023-07-15 15:12:53 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -4,8 +4,9 @@
 #pip install -r req.txt
 #pip freeze > req.txt
 chmod u+x *.sh *.py
+l_dir=pwd
 
-git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/react_sampl
+git config --global --add safe.directory $l_dir
 #git clone -o react_sampl https://github.com/PenguinLW/react_sampl.git
 #git config --global user.email "diana1997525@gmail.com"
 #git config --global user.name "PenguinLW"
