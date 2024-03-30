ecef537bbdfdc78c291b6042be51b5d17dd187be
PenguinLW <diana1997525@gmail.com>
2024-03-30 23:51:38 +0800

    PenguinL

---
+++ tmp_sampl/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const content = [
+    {title: "html", text: "html"},
+    { title: "css", text: "css"},
+    {title: "js", text: "js"}
+]
+
+export default function App() {
+    const [active, setActive] = useState(null);
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+        <ul>
+            {content.map((item, index) => (
+                <li
+                    key={item.title}
+                    onClick={() => setActive(index)}
+                    className={index === active ? "active" : null}
+                >
+                    <h3>{item.title}</h3>
+                    <div className="content">{item.text}</div>
+                </li>
+            ))}
+        </ul>
+    </div>
+  );
+}
